d7bcded44c9e23c983fb7336544afedbffef935f
Ben Hakim <ben@hakim.co.il>
2017-01-03 01:35:42 +0200

    Clients Fixes
    1. fix libs unused.
    2. fix cd command. 
    3. few changes

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 #1/usr/bin/python
 from socket import socket, AF_INET, SOCK_STREAM,error
 from subprocess import Popen, PIPE
-from time import sleep
-from sys import exit as exitapp
-from platform import dist as ostype
+from platform import system,dist
+from os import getcwd
 
 buffer_size = 4096
 host = '127.0.0.1'
 port = 1234
 max_timeout = 10
+cwd = getcwd()
 sock = socket(AF_INET , SOCK_STREAM)
 sock.settimeout(max_timeout)
 
@@ -16,6 +16,7 @@ try:
     sock.connect((host, port))
 except error as msg:
     print "Socket Error: %s" % msg
+
 while True:
     try:
         data = sock.recv(buffer_size)
@@ -34,18 +35,23 @@ while True:
                     break
         else:
             if "package" in data:
-                if ostype()[0] == "centos":
+                if dist()[0] == "centos":
                     data = data.replace("package", "yum")
-                elif ostype()[0] == "debian":
+                elif dist()[0] == "debian":
                     data = data.replace("package", "apt-get")
-            proc = Popen(data, shell=True, stdin=None, stdout=PIPE, stderr=PIPE,executable="/bin/bash")
+                elif system() == "Darwin":
+                    data = ""
+                    sock.send("not suppported on mac osx! \n")
+            if "cd" in data:
+                cwd = data.replace("cd ", "")
+            proc = Popen(data, shell=True, stdin=None, stdout=PIPE, stderr=PIPE, executable="/bin/bash", cwd=cwd)
             out, err = proc.communicate()
             if err:
                 sock.send(err)
             elif out and not err:
                 sock.send(out)
             elif out == "" and not err:
-                sock.send("empty return...")
+                sock.send("Nothing to show ! \n")
     except Exception as e:
         continue
 sock.close()
